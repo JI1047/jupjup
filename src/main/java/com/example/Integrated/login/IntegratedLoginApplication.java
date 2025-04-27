030a3b0aae6dc0f741af2a700ddf7e64006e7ef5
JI SEOKIM <kjs001004@naver.com>
2025-04-27 13:11:11 +0900

    feat: RDS 연결 및 기본 로그인 화면 설정 완료

---
+++ src/main/java/com/example/Integrated/login/IntegratedLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.Integrated.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratedLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratedLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Integrated/login/IntegratedLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Integrated.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratedLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
